9068ff365aff9cea10c4327c5d05b705d2deb037
vlad-sp <vladimirsopotlyanov@gmail.com>
2015-12-11 13:02:08 +0200

    Added image model

---
+++ Baudi/Common/ConvertImageFileToByteArray/ImageConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace ConvertImageFileToByteArray
+{
+    using System.IO;
+
+    public class ImageConverter
+    {
+        static void Main()
+        {
+            var filePath = "../../audi log.jpg";
+
+            var result = ConvertFileToByte(filePath);
+
+           // Console.WriteLine(string.Join(" ", result));
+             ConvertByteToFile("../../new.jpeg", result);
+        }
+
+        private static byte[] ConvertFileToByte(string filename)
+        {
+            var bytes = File.ReadAllBytes(filename);
+            return bytes;
+        }
+
+        private static void ConvertByteToFile(string path, byte[] bytes)
+        {
+            File.WriteAllBytes(path, bytes);
+        }
+    }
+}
